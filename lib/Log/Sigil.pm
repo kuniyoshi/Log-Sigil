5a3cd349fe31d85b6b666e4ebd244812c7f9c774
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2011-04-29 04:34:10 +0900

    initial commit

---
+++ lib/Log/Sigil.pm
@@ -0,0 +1,32 @@
+package Log::Sigil;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Log::Sigil -
+
+=head1 SYNOPSIS
+
+  use Log::Sigil;
+
+=head1 DESCRIPTION
+
+Log::Sigil is
+
+=head1 AUTHOR
+
+kuniyoshi kouji E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
